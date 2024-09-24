58b6dd42142f039557408a08ece571f6a47c88cd
qwerasdfxzdcv <cshxx992@gmail.com>
2024-09-24 14:03:28 +0900

    overriding 추가

--- 0924/src/Person2.java
+++ 0924/src/Person2.java
@@ -6,6 +6,11 @@ public class Person2 {
         this.name = name;
         this.age = age;
     }
+    //이미 존재하는것을 바꾸는 것을 overriding 이라고 함
+    @Override
+    public String toString() { //이미 존재하던 toString 을 바꾼 것
+        return "이름 : " + name + "(" + age + ")";
+    }
     //이름은 같지만 아규먼트를 다르게 설계해서 재사용성을 늘리는 이런 방식을 overloading 이라고 한다.
     //이름만 넣고 나이를 비공개로 하고싶을때 아래 생성자를 사용
     public Person2(String name) {
